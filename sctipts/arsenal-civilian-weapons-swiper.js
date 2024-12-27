c288f27a68d30b96a73e85b6de18ff90a84d8a35
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-27 11:14:01 +0500

    arsenal-civilian-weapons all format

--- sctipts/arsenal-civilian-weapons-swiper.js
+++ sctipts/arsenal-civilian-weapons-swiper.js
@@ -1,7 +1,24 @@
 var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 4,
-    spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 11,
     speed: 500,
+
+    grid: {
+        rows: 2,
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 640px
+        608: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            grid: {
+                rows: 1,
+            },
+        }
+    },
+
     pagination: {
         el: ".swiper-pagination",
         type: "progressbar",
