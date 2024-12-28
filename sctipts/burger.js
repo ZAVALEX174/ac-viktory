053375024ae103d4553d5039dc057635f2654359
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-28 09:40:29 +0500

    all sections

--- sctipts/burger.js
+++ sctipts/burger.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const openBergerBtn = document.querySelector('.burger-menu');
+    const openBergerFooterBtn = document.querySelector('.burger-menu__footer');
     const menuBurger = document.querySelector('.header__navigation-box');
+    const menuBurgerFooter = document.querySelector('.footer__navigation-box');
     const bergerLine = menuBurger.querySelector('.burger-line');
 
     openBergerBtn.addEventListener("click", function() {
         menuBurger.classList.toggle('header__navigation-box--open');
         openBergerBtn.classList.toggle('burger-menu-active');
+        document.body.classList.toggle('body-hidden');
+    })
+
+    openBergerFooterBtn.addEventListener("click", function() {
+        menuBurgerFooter.classList.toggle('footer__navigation-box--open');
+        openBergerFooterBtn.classList.toggle('burger-menu-footer-active');
         document.body.classList.toggle('body-hidden');
     })
 
