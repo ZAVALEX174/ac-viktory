8b65447147c528fdcdb7d7428598ea4ba8e64430
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-10 14:38:35 +0500

    news popup fixed

--- sctipts/modals.js
+++ sctipts/modals.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const openPopupBtns = document.querySelectorAll('.news-body__text--link');
-    console.log(openPopupBtns);
+    // console.log(openPopupBtns);
 
     const closePopupBtn = document.querySelector('.btn-close');
-    console.log(closePopupBtn);
+    // console.log(closePopupBtn);
 
     const popup = document.querySelector('.popup');
-    console.log(popup);
+    // console.log(popup);
 
-    const popupWrap = document.querySelector('.popup__wrapper');
+    // const popupWrap = document.querySelector('.popup__wrapper');
 
     openPopupBtns.forEach(btn => {
         btn.addEventListener("click", (e) => {
@@ -22,12 +22,13 @@ document.addEventListener('DOMContentLoaded', function () {
     closePopupBtn.addEventListener("click", (e) => {
         e.preventDefault();
         popup.style.display = "none";
-        document.body.classList.add("over-hidden");
+        document.body.classList.remove("over-hidden");
     })
 
     $(document).click(function (e) {
         if ($(e.target).is('.popup')) {
             popup.style.display = "none";
+            document.body.classList.remove("over-hidden");
         }
     });
 });
