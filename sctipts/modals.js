058955073e355cb6752c32b7f2512adaf8fc4b8d
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-10 14:23:07 +0500

    news popup create

---
+++ sctipts/modals.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const openPopupBtns = document.querySelectorAll('.news-body__text--link');
+    console.log(openPopupBtns);
+
+    const closePopupBtn = document.querySelector('.btn-close');
+    console.log(closePopupBtn);
+
+    const popup = document.querySelector('.popup');
+    console.log(popup);
+
+    const popupWrap = document.querySelector('.popup__wrapper');
+
+    openPopupBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            popup.style.display = "flex";
+            document.body.classList.add("over-hidden");
+        })
+    })
+
+    closePopupBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        popup.style.display = "none";
+        document.body.classList.add("over-hidden");
+    })
+
+    $(document).click(function (e) {
+        if ($(e.target).is('.popup')) {
+            popup.style.display = "none";
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
